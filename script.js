41f9958f494925fc2e4ed9c111fe64887c5602b6
GAURAV SINGH <gauravsingh264209@gmail.com>
2023-09-07 18:13:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+api_url = 'https://dog.ceo/api/breeds/image/random';
+
+
+const fetchImage=()=>{
+    var xhrRequest= new XMLHttpRequest();
+
+    xhrRequest.onload=()=>{
+        console.log(xhrRequest.response);
+
+        var JSONresponse=JSON.parse(xhrRequest.response);
+        var imageUrl=JSONresponse.message;
+
+        $('img').attr('src' , imageUrl);
+    };
+
+    xhrRequest.open('get' , api_url);
+    xhrRequest.send();
+}
+
+
+$('.btn').click(fetchImage);   // jQuery used here 
